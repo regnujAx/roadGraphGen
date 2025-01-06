f636ddfec738a8c20f6e1fadec06dc85f02bc54f
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2025-01-06 11:27:49 +0100

    move methods to RGG_GraphGenerator class

--- roadGraphGen/graph_generator.py
+++ roadGraphGen/graph_generator.py
@@ -13,7 +13,7 @@ from roadGraphGen.roadGraphGen.tensor_field import TensorField
 
 class RGG_GraphGenerator():
     def __init__(self, width: int = 100, height: int = 100, seed: int = None):
-        # Create new global TensorField
+        # Create new global TensorField.
         self.field = TensorField()
 
         # Create new StreamlineParameters. Values used here are derived from testing and seem like a good baseline.
@@ -78,78 +78,71 @@ class RGG_GraphGenerator():
 
             t = time()
 
-            visualize_edges(self.graph)
-            visualize_nodes(self.graph)
+            self.visualize_edges()
+            self.visualize_nodes()
 
             print(f"Visualization of graph completed in {time() - t:.2f}s")
 
+    # Turn streamline sections of the graph into curves to visualize in Blender.
+    def visualize_edges(self, prefix=''):
+        try:
+            grid = bpy.data.collections[prefix + "grid"]
+            bpy.ops.object.select_all(action='DESELECT')
 
-# ------------------------------------------------------------------------
-#    Helper Method
-# ------------------------------------------------------------------------
+            for child in grid.children:
+                for obj in child.objects:
+                    obj.select_set(True)
 
-
-# Helper method to turn streamline sections of the graph into curves to visualize in Blender.
-def visualize_edges(graph: Graph, prefix=''):
-    try:
-        grid = bpy.data.collections[prefix + "grid"]
-        bpy.ops.object.select_all(action='DESELECT')
-
-        for child in grid.children:
-            for obj in child.objects:
+            for obj in grid.objects:
                 obj.select_set(True)
 
-        for obj in grid.objects:
-            obj.select_set(True)
+            bpy.ops.object.delete()
 
-        bpy.ops.object.delete()
+            for child in grid.children:
+                bpy.data.collections.remove(child)
+        except Exception:
+            grid = bpy.data.collections.new(prefix + "grid")
+            bpy.context.scene.collection.children.link(grid)
 
-        for child in grid.children:
-            bpy.data.collections.remove(child)
-    except Exception:
-        grid = bpy.data.collections.new(prefix + "grid")
-        bpy.context.scene.collection.children.link(grid)
+        for streamline in self.graph.streamline_sections:
+            sl = bpy.data.collections.new("streamline")
+            grid.children.link(sl)
 
-    for streamline in graph.streamline_sections:
-        sl = bpy.data.collections.new("streamline")
-        grid.children.link(sl)
+            for section in streamline:
+                curve = bpy.data.curves.new("section", 'CURVE')
+                curve.splines.new('BEZIER')
+                curve.splines.active.bezier_points.add(len(section) - 1)
+                obj = bpy.data.objects.new("section", curve)
+                sl.objects.link(obj)
 
-        for section in streamline:
-            curve = bpy.data.curves.new("section", 'CURVE')
-            curve.splines.new('BEZIER')
-            curve.splines.active.bezier_points.add(len(section) - 1)
-            obj = bpy.data.objects.new("section", curve)
-            sl.objects.link(obj)
+                for i in range(len(section)):
+                    curve.splines.active.bezier_points[i].co = section[i].to_3d()
+                    curve.splines.active.bezier_points[i].handle_right_type = 'VECTOR'
+                    curve.splines.active.bezier_points[i].handle_left_type = 'VECTOR'
 
-            for i in range(len(section)):
-                curve.splines.active.bezier_points[i].co = section[i].to_3d()
-                curve.splines.active.bezier_points[i].handle_right_type = 'VECTOR'
-                curve.splines.active.bezier_points[i].handle_left_type = 'VECTOR'
+    # Place cubes at node points of the generated graph.
+    def visualize_nodes(self, prefix=''):
+        try:
+            nodes = bpy.data.collections[prefix + "nodes"]
+            bpy.ops.object.select_all(action='DESELECT')
+            for obj in nodes.objects:
+                obj.select_set(True)
+            bpy.ops.object.delete()
+        except Exception:
+            nodes = bpy.data.collections.new(prefix + "nodes")
+            bpy.context.scene.collection.children.link(nodes)
 
+        cube_mesh = bpy.data.meshes.new("Basic_Cube")
+        bm = bmesh.new()
+        bmesh.ops.create_cube(bm, size=1.5)
+        bm.to_mesh(cube_mesh)
+        bm.free()
 
-# Helper method to place cubes at node points of the generated graph.
-def visualize_nodes(graph: Graph, prefix=''):
-    try:
-        nodes = bpy.data.collections[prefix + "nodes"]
-        bpy.ops.object.select_all(action='DESELECT')
-        for obj in nodes.objects:
-            obj.select_set(True)
-        bpy.ops.object.delete()
-    except Exception:
-        nodes = bpy.data.collections.new(prefix + "nodes")
-        bpy.context.scene.collection.children.link(nodes)
+        for graph_node in self.graph.nodes:
+            if not [*graph_node.neighbors]:
+                # Ignore the node if it has no neigbors
+                continue
 
-    cube_mesh = bpy.data.meshes.new('Basic_Cube')
-    bm = bmesh.new()
-    bmesh.ops.create_cube(bm, size=1.5)
-    bm.to_mesh(cube_mesh)
-    bm.free()
-
-    for graph_node in graph.nodes:
-        if not [*graph_node.neighbors]:
-            # Ignore the node if it has no neigbors
-            continue
-
-        node = bpy.data.objects.new("Node", cube_mesh)
-        nodes.objects.link(node)
-        node.location = graph_node.co.to_3d()
+            node = bpy.data.objects.new("Node", cube_mesh)
+            nodes.objects.link(node)
+            node.location = graph_node.co.to_3d()
